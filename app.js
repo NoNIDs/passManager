6d82f9fe51b0aabe6f88e9b08cb2c20f692965f6
vaysberg <willwayer13@gmail.com>
2020-11-02 23:42:50 +0200

    rollback

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/pass", require("./routes/passManager.routes"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
