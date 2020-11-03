2846baca17cb188ee4dd12abda207c1ad34eb79c
konstantinCheban <31002007+NoNIDs@users.noreply.github.com>
2020-11-03 11:41:54 +0200

    change to prod

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/pass", require("./routes/passManager.routes"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.use("/", express.static(path.join(__dirname, "..", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
   });
 }
 
